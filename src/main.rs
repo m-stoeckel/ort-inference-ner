63f0944693ff55c71ac9182d4bc24fa0f234f671
Manuel Stoeckel <manuel.stoeckel@em.uni-frankfurt.de>
2023-11-27 16:39:14 +0100

    feat(rust): Migrated to ort & test with big file

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,10 @@ use std::cmp::Ordering;
 use std::path::PathBuf;
 
 use anyhow::anyhow;
+use clap::{Parser, ValueEnum};
 use ndarray::{Array2, Axis};
 use ndarray_stats::QuantileExt;
-use onnxruntime::{
-    environment::Environment, tensor::OrtOwnedTensor, GraphOptimizationLevel, LoggingLevel,
-};
+use ort::{Session, CUDAExecutionProvider, ExecutionProvider, TensorRTExecutionProvider};
 use rust_tokenizers::tokenizer::{BertTokenizer, Tokenizer, TruncationStrategy};
 use rust_tokenizers::vocab::{BertVocab, Vocab};
 use rust_tokenizers::Mask;
@@ -91,84 +90,162 @@ impl Annotation {
     }
 }
 
-fn main() {
-    let environment = Environment::builder()
-        .with_name("test")
-        .with_log_level(LoggingLevel::Verbose)
-        .build()
-        .unwrap();
-    let mut session = environment
-    .new_session_builder().unwrap()
-    .with_optimization_level(GraphOptimizationLevel::Basic).unwrap()
-    .with_number_threads(1).unwrap()
-    .with_model_from_file("/run/media/mastoeck/hot_storage/ONNX/distilbert-base-multilingual-cased-ner-hrl/model.onnx").unwrap();
-
-    let tokenizer = BertTokenizer::from_existing_vocab(
-        BertVocab::from_file(PathBuf::from("/run/media/mastoeck/hot_storage/ONNX/distilbert-base-multilingual-cased-ner-hrl/vocab.txt")).unwrap(),
-        false,
-        false,
-    );
-    let tokens = tokenizer.encode_list(
-        &[
-            "My name is Clara and I live in Berkeley, California.",
-            "I saw Barack Obama at the White House today.",
-            "Ich habe gestern die Goethe Universität in Frankfurt am Main besucht.",
-            "Nader Jokhadar had given Syria the lead with a well-struck header in the seventh minute."
-        ],
-        512,
-        &TruncationStrategy::LongestFirst,
-        0,
-    );
-
-    let (input_ids, attention_mask) = get_input_ids_and_attention_masks(&tokens);
-    let input_ids = values_to_array2(input_ids).unwrap();
-    let attention_mask = values_to_array2(attention_mask).unwrap();
-    let input_tensor = vec![input_ids, attention_mask];
-
-    let outputs: Vec<OrtOwnedTensor<f32, _>> = session.run(input_tensor).unwrap();
-    let preds: Vec<Vec<_>> = argmax_axis(&outputs[0], 2).unwrap();
-
-    let mut annot: Vec<Vec<Annotation>> = Vec::new();
-    for (ts, ps) in std::iter::zip(tokens, preds) {
-        let mut s_annot: Vec<Annotation> = Vec::new();
-        for ((t, m), p) in std::iter::zip(std::iter::zip(ts.token_offsets, ts.mask), ps) {
-            if p == 0 {
-                continue;
-            }
-            match (t, m) {
-                (Some(o), Mask::None | Mask::Begin) => {
-                    s_annot.push(Annotation::new(p, o.begin, o.end))
-                }
-                (Some(o), Mask::Continuation) => s_annot.last_mut().unwrap().end = o.end,
-                _ => continue,
-            };
-        }
-        annot.push(s_annot);
-    }
-    let json_string = serde_json::to_string_pretty(&annot).unwrap();
-    println!("{json_string}");
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum, Debug)]
+enum ImplementedProviders{
+    CPU,
+    CUDA,
+    TensorRT,
 }
 
-fn argmax_axis(
-    outputs: &OrtOwnedTensor<'_, '_, f32, ndarray::Dim<ndarray::IxDynImpl>>,
-    dim: usize,
-) -> Result<Vec<Vec<usize>>, ndarray_stats::errors::MinMaxError> {
-    outputs
-        .map_axis(Axis(dim), |v| v.argmax())
-        .rows()
-        .into_iter()
-        .map(|r| r.to_vec().into_iter().collect::<Result<Vec<_>, _>>())
-        .collect()
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(short, long, default_value="cpu")]
+    provider: ImplementedProviders,
+
+    #[arg(short, long, default_value_t = 0)]
+    device: u8,
+}
+
+fn main() -> anyhow::Result<()> {
+    let args = Args::parse();
+    
+    let builder = Session::builder()?;
+
+    match args.provider {
+        ImplementedProviders::CPU => (),
+        ImplementedProviders::CUDA => {
+            let cuda = CUDAExecutionProvider::default();
+            if let Err(err) = cuda.register(&builder) {
+                Err(anyhow!("Failed to register CUDA execution provider: {err:?}"))?
+            }
+        }
+        ImplementedProviders::TensorRT => {
+            let tensor = TensorRTExecutionProvider::default();
+            if let Err(err) = tensor.register(&builder) {
+                Err(anyhow!("Failed to register TensorRT execution provider: {err:?}"))?
+            }
+        },
+
+    }
+
+    ort::init()
+        // .with_execution_providers([CPUExecutionProvider::default().build()])
+        .commit()?;
+
+    let session = builder
+		// .with_optimization_level(GraphOptimizationLevel::Level1)?
+        .with_parallel_execution(true)?
+		.with_intra_threads(12)?
+        .with_model_from_file("/run/media/mastoeck/hot_storage/ONNX/distilbert-base-multilingual-cased-ner-hrl/model.onnx")?;
+
+    let tokenizer = BertTokenizer::from_existing_vocab(
+        BertVocab::from_file(PathBuf::from("/run/media/mastoeck/hot_storage/ONNX/distilbert-base-multilingual-cased-ner-hrl/vocab.txt"))?,
+        false,
+        false,
+    );
+    let corpus: Vec<String> =
+        std::fs::read_to_string("/hot_storage/Data/Leipzig/deu/deu_wikipedia_2021_10K-1k_shuf.txt")
+            .unwrap()
+            .lines()
+            .map(String::from)
+            .filter(|s| !s.is_empty())
+            .collect();
+    // let corpus = [
+    //     "My name is Clara and I live in Berkeley, California.",
+    //     "I saw Barack Obama at the White House today.",
+    //     "Ich habe gestern die Goethe Universität in Frankfurt am Main besucht.",
+    //     "Nader Jokhadar had given Syria the lead with a well-struck header in the seventh minute.",
+    // ];
+
+    let annot: Vec<Vec<Annotation>> = corpus
+        .chunks(128)
+        .map(|batch| {
+            let tokens = tokenizer.encode_list(
+                batch,
+                512,
+                &TruncationStrategy::LongestFirst,
+                0,
+            );
+
+            let (input_ids, attention_mask) = get_input_ids_and_attention_masks(&tokens);
+            let input_ids:Array2<i64> = values_to_array2(input_ids).unwrap();
+            let attention_mask:Array2<i64> = values_to_array2(attention_mask).unwrap();
+
+            (input_ids, attention_mask, tokens)
+        })
+        // .into_iter()
+        // .map(|batch| {
+        //     let tokens = tokenizer.encode(
+        //         &batch,
+        //         None,
+        //         512,
+        //         &TruncationStrategy::LongestFirst,
+        //         0,
+        //     );
+        //     let tokens = vec![tokens];
+
+        //     let (input_ids, attention_mask) = get_input_ids_and_attention_masks(&tokens);
+        //     let input_ids:Array2<i64> = values_to_array2(input_ids).unwrap();
+        //     let attention_mask:Array2<i64> = values_to_array2(attention_mask).unwrap();
+
+        //     (input_ids, attention_mask, tokens)
+        // })
+        .map(|(input_ids, attention_mask, tokens)|{
+            let outputs  = session.run(ort::inputs![input_ids, attention_mask]?)?;
+
+            let preds: Vec<Vec<usize>> = outputs[0]
+                .extract_tensor::<f32>()?
+                .view()
+                .map_axis(Axis(2), |v| v.argmax())
+                .rows()
+                .into_iter()
+                .map(|r| {
+                    r.to_vec().into_iter().collect::<Result<Vec<usize>, _>>()
+                })
+                .collect::<Result<Vec<Vec<usize>>, _>>()?;
+            
+            anyhow::Ok((preds, tokens))
+        })
+        .filter_map(anyhow::Result::ok)
+        .flat_map(|(preds, tokens)| std::iter::zip(tokens, preds))
+        .map(|(ts, ps)|{
+            let mut annotations: Vec<Annotation> = Vec::new();
+            for ((t, m), p) in std::iter::zip(std::iter::zip(&ts.token_offsets, &ts.mask), &ps) {
+                match (t, m) {
+                    (_, Mask::Special) => continue,
+                    (Some(o), Mask::Continuation) => match annotations.last_mut() {
+                        Some(last_annotation) => last_annotation.end = o.end,
+                        None => panic!("Got a continuation token without preceeding ordinary token! {ts:#?} {ps:#?}"),
+                    },
+                    (Some(o), _) => {
+                        annotations.push(Annotation::new(*p, o.begin, o.end))
+                    }
+                    _ => continue,
+                };
+            }
+            
+            annotations
+        }).map(|annotations| {
+            annotations.into_iter()
+                .filter(|a|!matches!(a.label, Label::O))
+                .collect::<Vec<Annotation>>()
+        })
+        .filter(|annotations| !annotations.is_empty())
+        .collect();
+    let json_string = serde_json::to_string_pretty(&annot).unwrap();
+    println!("{json_string}");
+    Ok(())
 }
 
 fn get_input_ids_and_attention_masks(
-    tokens: &Vec<rust_tokenizers::TokenizedInput>,
+    batch_encoding: &Vec<rust_tokenizers::TokenizedInput>,
 ) -> (Vec<Vec<i64>>, Vec<Vec<i64>>) {
-    let mut input_ids: Vec<Vec<i64>> = Vec::with_capacity(tokens.len());
-    let mut attention_mask: Vec<Vec<i64>> = Vec::with_capacity(tokens.len());
-    for t in tokens.iter() {
-        input_ids.push(t.token_ids.clone());
-        attention_mask.push(t.token_ids.iter().map(|v| (*v > 0) as i64).collect());
+    let mut input_ids: Vec<Vec<i64>> = Vec::with_capacity(batch_encoding.len());
+    let mut attention_mask: Vec<Vec<i64>> = Vec::with_capacity(batch_encoding.len());
+    for seq_encoding in batch_encoding.iter() {
+        input_ids.push(seq_encoding.token_ids.clone());
+        attention_mask.push(vec![1_i64; seq_encoding.token_ids.len()]);
     }
     (input_ids, attention_mask)
 }
